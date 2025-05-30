a2de1991c16a2a5d8f3b55769750c5937ecd4b19
SempaiTakoo <idl76h@gmail.com>
2025-05-30 23:05:56 +0300

    Add kafka-net

--- bash.sh
+++ bash.sh
@@ -1,3 +1,6 @@
+docker network create kafka-net
+
+
 docker compose -f services/order_service/docker-compose.yml up -d
 # docker compose -f services/product_service/docker-compose.yml up -d
 docker compose -f services/user_service/docker-compose.yml up -d
