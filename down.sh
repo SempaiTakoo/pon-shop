6f9303f7865525187c4705e56aa66133c4f1d1b6
Ivan Kochkozharov <tegusigaalpa@gmail.com>
2025-05-31 21:47:39 +0300

    Fix ports issue

--- down.sh
+++ down.sh
@@ -1,3 +1,4 @@
 docker compose -f services/order_service/docker-compose.yml down
 docker compose -f services/user_service/docker-compose.yml down
+docker compose -f services/review_service/docker-compose.yml down
 docker compose -f services/kafka_service/docker-compose.yml down
